4fa16a81be1525a983cfd32a6c95c6093c014968
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-09-26 17:42:49 -0400

    installed react-iframe

--- src/components/project1.js
+++ src/components/project1.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import './project1.css';
+import WebView from 'react-electron-web-view';
 
 class Project1 extends Component {
   render() {
     return (
       <article className='project-1'>
         <h2>Project 1</h2>
+        <WebView id="beachtides" src="https://www.google.com" />
       </article>
     );
   }
 }
 
 export default Project1;
+
+// https://www.beachtides.us/
