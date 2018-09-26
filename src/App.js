518543d4697e70ab7a1e7efd325dd1e6f42d55a8
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-09-26 08:43:10 -0400

    background image on landing set

--- src/App.js
+++ src/App.js
@@ -10,9 +10,12 @@ class App extends Component {
     return (
       <div className="App">
         <LandingPage />
+        <div>
+          <h2>Projects</h2>
+          <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</p>
+        </div>
       </div>
     );
   }
 }
-
 export default App;
--- src/components/landing.js
+++ src/components/landing.js
@@ -1,23 +1,28 @@
 import React, { Component } from 'react';
 import Typing from 'react-typing-animation';
 import './landing.css';
+import earthUpdsideDown from '../images/earth-upside-down.jpg';
+
 
 class LandingPage extends Component {
   render() {
     return (
       <div className="landing-page">
-        <h1>Hi, I'm Jacob.</h1>
-        <Typing className='what-i-do' loop={true} hide cursor={true}>
-          <h1>I am a Full-Stack Web Developer</h1>
-          <Typing.Delay ms={1500} />
-          <Typing.Backspace count={31} />
-          <h1>I build API servers</h1>
-          <Typing.Delay ms={1500} />
-          <Typing.Backspace count={31} />
-          <h1>I am a leader</h1>
-          <Typing.Delay ms={1500} />
-          <Typing.Backspace count={31} />
-        </Typing>
+        <img className='earth-landing-image' src={earthUpdsideDown} />
+        <header className='header'>
+          <h1>Hello, I'm Jacob.</h1>
+          <Typing className='what-i-do' loop={true} hide cursor={true}>
+            <h1>I am a Full-Stack Web Developer</h1>
+            <Typing.Delay ms={1500} />
+            <Typing.Backspace count={31} />
+            <h1>I build API servers</h1>
+            <Typing.Delay ms={1500} />
+            <Typing.Backspace count={31} />
+            <h1>I am a leader</h1>
+            <Typing.Delay ms={1500} />
+            <Typing.Backspace count={31} />
+          </Typing>
+        </header>
       </div>
     );
   }
