c788028e60c5a565e96cd8c7b1936edab76d4c84
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-12-04 14:53:41 -0500

    added li element to about.js component

--- src/components/about.js
+++ src/components/about.js
@@ -14,6 +14,7 @@ class AboutMe extends Component {
             <li>&nbsp;&nbsp;Hi, I'm a full-stack web developer hailing from Massachusetts. I enjoy building client and server applications.</li>
             <li>&nbsp;&nbsp;I am mostly a self-taught developer, and I graduated from <a href='https://www.thinkful.com/bootcamp/web-development/full-time/' target="_blank" rel='noopener noreferrer' className='thinkful-link'>Thinkful</a>, A Full Stack Engineering Immersion School.</li> 
             <li>I program in JavaScript and I am comfortable with NodeJS and ReactJs.</li>
+            <li>I am looking to work in a company as a Full Stack, Front-End or Back-End Engineer.  I would like to work with a team of engineers in an agile environemnt.</li>
             <li>&nbsp;&nbsp;I am a highly ambitious self-starter with an entrepreneurial background in an e-commerce start-up, as well as revenue generation, early childhood education, and psychology.</li>
             <li>&nbsp;&nbsp;I am currently learning React-Native and GraphQL.</li>
             <li>&nbsp;&nbsp;In my spare time, I am building a project called Function Earth.  Function Earth is a native app that inspires users to be responsible and take care of our planet.</li>
