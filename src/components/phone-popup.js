eb00302504ec8cad44bac38f18a51c2fd95918a6
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2019-01-21 20:41:07 -0500

    reformated phone-popup.js

--- src/components/phone-popup.js
+++ src/components/phone-popup.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import './styles/phone-popup.css';
 
-export default class PhonePopup extends React.Component {
-  render() {
-    return (
-      <div className='popup'>
-        <div className='popup_inner'>
-          <h1>Mobile</h1>
-          <h1>508-244-1362</h1>
-          <button onClick={this.props.closePopup}>close me</button>
-        </div>
-      </div>
-    );
-  }
-}
+export default props => (
+  <div className='popup'>
+    <div className='popup_inner'>
+      <h1>Mobile</h1>
+      <h1>508-244-1362</h1>
+      <button onClick={props.closePopup}>close me</button>
+    </div>
+  </div>
+);
