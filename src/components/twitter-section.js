b1a24c0017f4721311045c916d2e5246035367e9
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2019-01-21 20:36:05 -0500

    reformated twitter.js code

--- src/components/twitter-section.js
+++ src/components/twitter-section.js
@@ -1,25 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Timeline } from 'react-twitter-widgets';
 import './styles/twitter-section.css';
 
-
-class Twitter extends Component {
-  render() {
-    return (
-      <section className='twitter' id='twitter'>
-        <h2 className='twitter-header'>Twitter</h2>
-        <div className='twitter-widget'>
-          <Timeline                     
-            dataSource={{
-              sourceType: "profile",
-              screenName: "jacobcmacinnis"
-            }}
-            options={{username: "jacobcmacinnis", height: '500px'}}
-          />
-        </div>
-      </section>
-    );
-  }
-}
-
-export default Twitter;
+export default () => (
+  <section className='twitter' id='twitter'>
+    <h2 className='twitter-header'>Twitter</h2>
+    <div className='twitter-widget'>
+      <Timeline                     
+        dataSource={{
+          sourceType: "profile",
+          screenName: "jacobcmacinnis"
+        }}
+        options={{username: "jacobcmacinnis", height: '500px'}}
+      />
+    </div>
+  </section>
+);
